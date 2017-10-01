390b0c80d0668477ec3edee2ee59499730b58314
franksonet <frankso.net@gmail.com>
2017-10-01 20:47:07 +0800

    trained a sucessful model which finishes the whole lap

--- CarND-Behavioral-Cloning-P3/model.py
+++ CarND-Behavioral-Cloning-P3/model.py
@@ -1,66 +1,82 @@
 import csv
 import cv2
 import numpy as np
+from sklearn.utils import shuffle
 
-lines = []
+samples = []
 
-datafolder = 'testdata'
+datafolder = 'data_1'
 imgfolder = 'IMG'
 
 with open('{}/driving_log.csv'.format(datafolder)) as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
-        lines.append(line)
+        samples.append(line)
 
-images = []
-measurements = []
-for line in lines:
-    for i in range(3):
-        source_path = line[i]
-        filename = source_path.split('/')[-1]
-        current_path = '{}/{}/'.format(datafolder, imgfolder) + filename
-        image = cv2.imread(current_path)
-        images.append(image)
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
-        measurement = float(line[3])
-        if i == 1:
-            measurement += 0.2
-        if i == 2:
-            measurement -= 0.2
-        measurements.append(measurement)
 
-augmented_images, augmented_measurements = [], []
-for image, measurement in zip(images, measurements):
-    augmented_images.append(image)
-    augmented_measurements.append(measurement)
-    augmented_images.append(cv2.flip(image, 1))
-    augmented_measurements.append(measurement * -1.0)
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1:
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset: offset + batch_size]
 
-X_train = np.array(augmented_images)
-y_train = np.array(augmented_measurements)
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                for i in range(3):
+                    source_path = batch_sample[i]
+                    filename = source_path.split('/')[-1]
+                    current_path = '{}/{}/'.format(datafolder, imgfolder) + filename
+                    image = cv2.imread(current_path)
+                    images.append(image)
+                    angle = float(batch_sample[3])
+                    if i == 1:
+                        angle += 0.2
+                    if i == 2:
+                        angle -= 0.2
+                    angles.append(angle)
 
-print(X_train.shape)
-print(y_train.shape)
+            augmented_images, augmented_angles = [], []
+            for image, angle in zip(images, angles):
+                augmented_images.append(image)
+                augmented_angles.append(angle)
+                augmented_images.append(cv2.flip(image, 1))
+                augmented_angles.append(angle * -1.0)
+
+            X_train = np.array(augmented_images)
+            y_train = np.array(augmented_angles)
+            yield shuffle(X_train, y_train)
 
 # img = X_train[10]
 # cv2.imshow('image', img)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 
+
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Cropping2D, Lambda
-from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import Convolution2D, Conv2D
 from keras.layers.pooling import MaxPooling2D
 
-input_shape = X_train.shape[1:]
+batch_size = 32
+
+
+train_generator = generator(train_samples, batch_size=batch_size)
+validation_generator = generator(validation_samples, batch_size=batch_size)
+
+input_shape = (160, 320, 3)
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=input_shape))
 model.add(Cropping2D(cropping=((70, 25), (0, 0))))
-model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu"))
-model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
-model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
-model.add(Convolution2D(64, 3, 3, activation="relu"))
-model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Conv2D(24, (5, 5), strides=(2, 2), activation="relu"))
+model.add(Conv2D(36, (5, 5), strides=(2, 2), activation="relu"))
+model.add(Conv2D(48, (5, 5), strides=(2, 2), activation="relu"))
+model.add(Conv2D(64, (3, 3), activation="relu"))
+model.add(Conv2D(64, (3, 3), activation="relu"))
 model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
@@ -68,7 +84,11 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+model.fit_generator(train_generator,
+                    steps_per_epoch=len(train_samples) / batch_size,
+                    validation_data=validation_generator,
+                    epochs=5,
+                    validation_steps=len(validation_samples) / batch_size)
 
 model.save('model.h5')
 exit()
